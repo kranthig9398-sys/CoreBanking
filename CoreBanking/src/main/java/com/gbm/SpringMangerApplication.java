d8101f003f4a44ef5b54ffec5354a4e658753e5c
kranthig9398-hub <kranthig9696@gmai.com>
2026-05-13 17:24:06 +0530

    Removed code for Twilo Service for whatsapp integration

--- CoreBanking/src/main/java/com/gbm/SpringMangerApplication.java
+++ CoreBanking/src/main/java/com/gbm/SpringMangerApplication.java
@@ -1,14 +1,9 @@
 package com.gbm;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import com.twilio.Twilio;
 
 
 @SpringBootApplication
@@ -17,17 +12,8 @@ public class SpringMangerApplication extends SpringBootServletInitializer {
 		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 			return application.sources(SpringMangerApplication.class);
 		}
-		@Value("${twilio.accountSid}")
-	    private String accountSid;
-
-	    @Value("${twilio.authToken}")
-	    private String authToken;
 		public static void main(String[] args) {
 			
 			SpringApplication.run(SpringMangerApplication.class, args);
 		}
-		 @PostConstruct
-		    public void initTwilio() {
-		        Twilio.init(accountSid, authToken);
-		    }
 }
